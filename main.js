3d10a029de1f55d16a90b873b4eb1bb3012dd856
Aimee Bachari <aimeebachari@gmail.com>
2018-07-18 14:11:00 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let rami = 88;
+let natoya = 92;
+let bill = 68;
+let susie = 100;
+let sasha = 82;
+let mohammed = 98;
+let jennifer = 94;
+let ken = 92;
+
+let totalJustReact = rami + natoya + bill + susie;
+
+let averageJustReact = totalJustReact / 4;
+
+let totalWeAreRuby = sasha + mohammed + jennifer + ken;
+
+let averageWeAreRuby = totalWeAreRuby / 4;
+
+let cohortTotal = totalJustReact + totalWeAreRuby;
+
+let cohortAverage = cohortTotal / 8;
+
+console.log(Math.round(averageJustReact));
+
+console.log(Math.round(averageWeAreRuby));
+
+console.log(Math.round(cohortAverage));
